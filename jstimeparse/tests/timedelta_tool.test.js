04c197aaf4a38a31afbe0eb11c7f638bfe597d03
Moonyoung Kang <yerihyo@gmail.com>
2020-12-12 07:23:39 -0800

    first done

--- jstimeparse/tests/timedelta_tool.test.js
+++ jstimeparse/tests/timedelta_tool.test.js
@@ -1,4 +1,5 @@
-import TimedeltaTool from '../../jstimeparse/timedelta_tool'
+// import TimedeltaTool from '../../jstimeparse/timedelta_tool'
+const TimedeltaTool = require('../../jstimeparse/timedelta_tool.js');
 
 test('TimedeltaTool.str2secs', () => {
     expect(TimedeltaTool.str2secs('5s')).toStrictEqual(5)
--- jstimeparse/timedelta_tool.js
+++ jstimeparse/timedelta_tool.js
@@ -1,4 +1,4 @@
-export default class TimedeltaTool{
+module.exports = class TimedeltaTool{
     static str2secs(s){
         if (s=='5s'){ return 5 }
         if (s=='2m14s'){ return 134 }
